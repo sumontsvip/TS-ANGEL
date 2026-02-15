34bce89630a9a4b146fd81747cd592a8a4151721
Trader Sumon <sekhmaniruddin823@gmail.com>
2026-02-15 11:49:28 +0530

    Update signal.js

--- api/signal.js
+++ api/signal.js
@@ -1,1 +1,9 @@
+export default function handler(req, res) {
+  const signals = ["BUY 🟢", "SELL 🔴"];
+  const random = signals[Math.floor(Math.random() * signals.length)];
 
+  res.status(200).json({
+    signal: random,
+    time: new Date().toLocaleTimeString()
+  });
+}
